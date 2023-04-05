738ef1746b0c1e1421779f328314944b40f66398
James Atkinson <james@Jamess-MacBook-Pro.local>
2023-04-05 15:47:02 +1200

    first commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/james/Desktop/DYDX/venv"
+set -gx VIRTUAL_ENV "/Users/james/DYDX/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
